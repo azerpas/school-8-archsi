183ee36f203407b155519a1346febc44cf0deed7
azerpas <xskylix@hotmail.fr>
2022-04-08 15:58:11 +0200

    first commit

---
+++ TP1/CorbaAdditionClient/src/StartClient.java
@@ -0,0 +1,47 @@
+package CorbaAdditionClient.src;
+
+/**
+ *
+ * @author imed
+ */
+import CorbaAdditionServer.src.AdditionApp.*;
+    
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import java.io.*;
+import java.util.*;
+    
+public class StartClient {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            ORB orb = ORB.init(args, null);
+            org.omg.CORBA.Object objRef =   orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            Addition addobj = (Addition) AdditionHelper.narrow(ncRef.resolve_str("ABC"));
+    
+                Scanner c=new Scanner(System.in);
+                System.out.println("Welcome to the addition system:");          		    
+                for(;;){
+                System.out.println("Enter a:");
+                String aa = c.nextLine();
+                System.out.println("Enter b:");
+                String bb = c.nextLine();
+                int a=Integer.parseInt(aa);
+                int b=Integer.parseInt(bb);
+                int r=addobj.add(a,b);
+                System.out.println("The result for addition is : "+r);
+                System.out.println("-----------------------------------");
+                }
+        }
+        catch (Exception e) {
+                System.out.println("Hello Client exception: " + e);
+                e.printStackTrace();
+        }
+    
+    }
+}
---
+++ TP1/CorbaAdditionServer/src/AdditionObj.java
@@ -0,0 +1,32 @@
+package CorbaAdditionServer.src;
+
+/**
+ *
+ * @author imed
+ */
+import CorbaAdditionServer.src.AdditionApp.*;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+import java.util.Properties;
+  
+class AdditionObj extends AdditionPOA {
+  private ORB orb;
+  
+  public void setORB(ORB orb_val) {
+    orb = orb_val; 
+  }
+  
+  // implement add() method
+  public int add(int a, int b) {
+    int r=a+b;
+    return r;
+  }
+  
+  // implement shutdown() method
+  public void shutdown() {
+    orb.shutdown(false);
+  }
+}
